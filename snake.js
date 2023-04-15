d5049c10b0b7f540a4fe76c421a2a5910da986ad
CheBar1 <chebarclay@live.com.au>
2023-04-15 10:59:23 +0930

    added game.js and snake.js

---
+++ snake.js
@@ -0,0 +1,14 @@
+export const SNAKE_SPEED = 1
+const snakeBody = [{x: 11, y: 11}]
+
+export function update() {
+    console.log('update snake');
+}
+
+export function draw() {
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div')
+        snakeElement.style.gridRowStart = segment.x
+        snakeElement.style.gridColumnStart = segment.y
+    })     
+}
