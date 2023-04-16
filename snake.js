91e9f2228179da6b3867467ec2f8a082b0425b13
CheBar1 <chebarclay@live.com.au>
2023-04-16 17:01:05 +0930

    updated snake.js and game.js so snake can be seen on gameBoard

--- snake.js
+++ snake.js
@@ -5,10 +5,12 @@ export function update() {
     console.log('update snake');
 }
 
-export function draw() {
+export function draw(gameBoard) {
     snakeBody.forEach(segment => {
         const snakeElement = document.createElement('div')
         snakeElement.style.gridRowStart = segment.x
         snakeElement.style.gridColumnStart = segment.y
+        snakeElement.classList.add('snake')
+        gameBoard.appendChild(snakeElement)
     })     
 }
